b0e355a92c5e557a2693df0f52e51142c0a0e1cc
magiccrafting <jh.12@outlook.com>
2019-04-17 14:54:20 -0500

    Clean up

--- socialapp-client/src/components/EditDetails.js
+++ socialapp-client/src/components/EditDetails.js
@@ -2,17 +2,22 @@ import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 
+// Redux
 import { connect } from "react-redux";
 import { editUserDetails } from "../redux/actions/userActions";
+
+// MUI
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import Tooltip from "@material-ui/core/Tooltip";
+
+// Icons
 import EditIcon from "@material-ui/icons/Edit";
 import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
 
 const styles = theme => ({
   ...theme
